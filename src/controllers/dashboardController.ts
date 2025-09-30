4c5bb463dd78a7a5d30312dbeeaca8a7573472ae
jayanthbr29 <jayanthbr@digi9.co.in>
2025-09-30 15:03:47 +0530

    dashboard stats api

---
+++ src/controllers/dashboardController.ts
@@ -0,0 +1,46 @@
+import {
+    collection,
+    doc,
+    addDoc,
+    getDocs,
+    getDoc,
+    deleteDoc,
+    Timestamp,
+    where,
+    query,
+    orderBy,
+    QueryConstraint,
+} from "firebase/firestore";
+import type { Request, Response } from "express";
+import db from "../configs/firebase.js";
+import { AppError, handleError } from "../utils/errorHandler.js";
+
+export const getDashboardStats = async (req: Request, res: Response) => {
+    try {
+        const usersRef = collection(db, "Users")
+        const advertisementRef = collection(db, "Advertisement");
+        const adminDataRef = collection(db, "admindata");
+        const approved_addRef = collection(db, "approved_add");
+
+        const usersSnapshot = await getDocs(query(usersRef));
+        const usersWithoutVendorField = usersSnapshot.docs.filter(doc =>
+            !('isVendor' in doc.data())
+        );
+        const vendorSnapshot = await getDocs(query(usersRef, where("isVendor", "==", true)))
+        const advertisementSnapshot = await getDocs(advertisementRef);
+        const adminDataSnapshot = await getDocs(adminDataRef);
+        const approved_addSnapshot = await getDocs(approved_addRef);
+
+
+        const totalUsers = usersWithoutVendorField.length;
+        const totalVendors = vendorSnapshot.size;
+        const totalRo = advertisementSnapshot.size;
+        const remainingBudget = adminDataSnapshot.docs[0]?.data()?.Budget || 0;
+        const totalNoteSheets = approved_addSnapshot.size;
+
+        res.status(200).json({ totalUsers, totalVendors, totalRo, remainingBudget, totalNoteSheets });
+    } catch (error: Error | any) {
+        console.error("Error in getDashboardStats:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
