dbbf76527882f6ee77ee767ed846ccbd90e8e0ef
jayanthbr29 <jayanthbr@digi9.co.in>
2025-10-13 12:49:57 +0530

    deputy IR send again and approve putup request action

--- dist/src/controllers/InvoiceRequestController.d.ts
+++ dist/src/controllers/InvoiceRequestController.d.ts
@@ -2,4 +2,6 @@ import type { Request, Response } from "express";
 export declare const getInvoiceRequestCount: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const createInvoice: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const editInvoice: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const deputyInvoiceSendBack: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const deputyApproveInvoiceRequestPutUp: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 //# sourceMappingURL=InvoiceRequestController.d.ts.map
