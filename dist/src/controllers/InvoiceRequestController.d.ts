af3a3c78149f4c761714518c1d81f583e47c7bba
jayanthbr29 <jayanthbr@digi9.co.in>
2025-10-13 15:51:11 +0530

    assistant approve and submit action

--- dist/src/controllers/InvoiceRequestController.d.ts
+++ dist/src/controllers/InvoiceRequestController.d.ts
@@ -5,4 +5,6 @@ export declare const editInvoice: (req: Request, res: Response) => Promise<Respo
 export declare const deputyInvoiceSendBack: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const deputyApproveInvoiceRequestPutUp: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 export declare const deputyApproveInvoiceRequestSendForward: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const assistantApproveInvoiceRequest: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
+export declare const assistantSubmitInvoiceRequest: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
 //# sourceMappingURL=InvoiceRequestController.d.ts.map
--- dist/src/routes/InvoiceRequestRoutes.js
+++ dist/src/routes/InvoiceRequestRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createInvoice, deputyApproveInvoiceRequestPutUp, deputyApproveInvoiceRequestSendForward, deputyInvoiceSendBack, editInvoice, getInvoiceRequestCount } from "../controllers/InvoiceRequestController.js";
+import { assistantApproveInvoiceRequest, assistantSubmitInvoiceRequest, createInvoice, deputyApproveInvoiceRequestPutUp, deputyApproveInvoiceRequestSendForward, deputyInvoiceSendBack, editInvoice, getInvoiceRequestCount } from "../controllers/InvoiceRequestController.js";
 const router = express.Router();
 router.get("/stats/invoiceRequest/count/year/:year", getInvoiceRequestCount);
 router.post("/create/invoiceRequest/byVendor", createInvoice);
@@ -7,5 +7,7 @@ router.put("/edit/invoiceRequest/byVendor", editInvoice);
 router.patch("/sendAgain/invoiceRequest/byDeputy", deputyInvoiceSendBack);
 router.patch("/approve/putup/invoiceRequest/byDeputy", deputyApproveInvoiceRequestPutUp);
 router.patch("/approve/sendForward/invoiceRequest/byDeputy", deputyApproveInvoiceRequestSendForward);
+router.patch("/approve/invoiceRequest/byAssistant", assistantApproveInvoiceRequest);
+router.put("/submit/invoiceRequest/byAssistant", assistantSubmitInvoiceRequest);
 export default router;
 //# sourceMappingURL=InvoiceRequestRoutes.js.map
